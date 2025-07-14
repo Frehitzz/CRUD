79e964f71d8448cc929ab3965a2fd02bbdfbd8f6
Frehitzz <fritzharlydegamo@gmail.com>
2025-07-14 22:33:15 +0800

    created a form for sending email

---
+++ forgot_pass.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/forgot_pass.css">
+</head>
+<body>
+    <div class="container">
+        <form>
+            <input type="text" class="email_input" name="email" placeholder="Enter your email"><br>
+            <button type="submit" class="submit_button">Send Reset Link</button>
+        </form>
+    </div>
+</body>
+</html>
