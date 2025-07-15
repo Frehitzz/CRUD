25c48e572ffaaf0af7cc00a2fe7b1680000cbd2f
Frehitzz <fritzharlydegamo@gmail.com>
2025-07-15 23:54:35 +0800

    this is for testing the email sending

---
+++ test_mail.php
@@ -0,0 +1,31 @@
+<?php
+require 'src/PHPMailer.php';
+require 'src/SMTP.php';
+require 'src/Exception.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+$mail = new PHPMailer(true);
+
+try {
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'yourgmail@gmail.com';
+    $mail->Password = 'your_app_password';
+    $mail->SMTPSecure = 'tls';
+    $mail->Port = 587;
+
+    $mail->setFrom('yourgmail@gmail.com', 'FritzApp');
+    $mail->addAddress('yourdestination@gmail.com'); // <- your test email
+
+    $mail->isHTML(true);
+    $mail->Subject = 'Test Email';
+    $mail->Body    = 'This is a test email from PHPMailer.';
+
+    $mail->send();
+    echo 'Message sent!';
+} catch (Exception $e) {
+    echo "Mailer Error: " . $mail->ErrorInfo;
+}
