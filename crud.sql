a2e90d6369e0b5de4d36e80e4325bb706afd14ef
Frehitzz <fritzharlydegamo@gmail.com>
2025-07-15 23:50:47 +0800

    added an expire token column

--- crud.sql
+++ crud.sql
@@ -6,3 +6,9 @@ CREATE TABLE `users` (
   `pass` varchar(255) NOT NULL,
   `created_at` datetime NOT NULL DEFAULT curtime()
 )
+
+-- adding a reset_token column to the database
+
+ALTER TABLE `users`
+ADD COLUMN `reset_token` VARCHAR(64) DEFAULT NULL;
+ADD token_expire DATETIME DEFAULT NULL;
