77fb09b884fbe1407e85eebc8b38f79c35cf7fd9
Frehitzz <fritzharlydegamo@gmail.com>
2025-07-19 14:43:01 +0800

    added style for message success

--- reset_password.php
+++ reset_password.php
@@ -24,13 +24,19 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $newPassword = $_POST['new_password'];
     $confirmPassword = $_POST['confirm_password'];
 
+    // if empty
     if (empty($newPassword) || empty($confirmPassword)) {
         $message = "Please fill in all fields.";
-    } elseif ($newPassword !== $confirmPassword) {
+    } 
+    // if the pass is not same on confirm pass
+    elseif ($newPassword !== $confirmPassword) {
         $message = "Passwords do not match.";
-    } elseif (strlen($newPassword) < 6) {
+    }
+    // password limit
+    elseif (strlen($newPassword) < 6) {
         $message = "Password must be at least 6 characters.";
     } else {
+        // if there is no problem
         // Find user again by token
         $stmt = $pdo->prepare("SELECT * FROM users WHERE reset_token = :token AND token_expire > NOW()");
         $stmt->bindParam(":token", $token);
@@ -58,11 +64,11 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 <head>
   <meta charset="UTF-8">
   <title>Reset Password</title>
-  <link rel="stylesheet" href="css/reset_password.css">
+  <link rel="stylesheet" href="css/reset_pass.css">
+  <link rel="stylesheet" href="css/signup.css">
 </head>
 <body>
   <div class="container">
-    <h2>Reset Password</h2>
 
     <?php if (!empty($message)): ?>
       <p class="form-error"><?= $message ?></p>
@@ -70,13 +76,20 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
     <?php if (isset($_GET['token']) && empty($message)): ?>
     <form action="reset_password.php" method="POST">
+      <h2>Reset Password</h2>
+      <!-- keeps the token available for the POST request. -->
       <input type="hidden" name="token" value="<?= htmlspecialchars($_GET['token']); ?>">
-      <input type="password" name="new_password" placeholder="New Password" required><br>
-      <input type="password" name="confirm_password" placeholder="Confirm Password" required><br>
+      <input class="input_pass" id="reset_pass" type="password" name="new_password" placeholder="New Password" required oninput="pass_strength(this.value)">
+      <div class="strength-indicator">
+          <div class="strength-bar" id="strengthBar"></div><!--password bar-->
+      </div>
+      <input class="input_pass" id="reset_conpass" class="con_pass" type="password" name="confirm_password" placeholder="Confirm Password" required><br>
+      <input class="mycheckb" type="checkbox" onclick="passcheckb_manage()"><label>Show Password</label>
       <button type="submit">Reset Password</button>
     </form>
     <?php endif; ?>
   </div>
+  <script src="js/resetpass.js"></script>
 </body>
 </html>
 
