6a36ed903e4661ac40cd48d14fff08e0683ea631
Frehitzz <fritzharlydegamo@gmail.com>
2025-07-24 15:05:12 +0800

    add: table for inventory

--- crud.sql
+++ crud.sql
@@ -12,3 +12,14 @@ CREATE TABLE `users` (
 ALTER TABLE `users`
 ADD COLUMN `reset_token` VARCHAR(64) DEFAULT NULL;
 ADD token_expire DATETIME DEFAULT NULL;
+
+-- Inventory table
+
+CREATE TABLE inventory(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  item_name VARCHAR(100) NOT NULL,
+  category VARCHAR(100),
+  quantity INT NOT NULL,
+  sta_tus VARCHAR(20),
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
